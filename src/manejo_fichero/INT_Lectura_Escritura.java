6f645b1a72a1bed728e27bf0e5d549b9c2ff9762
Felipe Andrés Vásquez Tort <felipevasquez.tort@gmail.com>
2015-10-28 12:52:55 +0100

    Agregado el javadoc del metodo registro_actividad.

--- src/manejo_fichero/INT_Lectura_Escritura.java
+++ src/manejo_fichero/INT_Lectura_Escritura.java
@@ -1,5 +1,16 @@
 package manejo_fichero;
 
 public interface INT_Lectura_Escritura {
+	/**
+	 * Method which aims to create a file where a record of the activity that
+	 * the program have.
+	 * 
+	 * @author Bufigol
+	 * @param ruta_archvio
+	 *            The path where to save the log file, or if the path of a file
+	 *            which already exists for charging.
+	 * @param linea_entrada
+	 *            The new information that will be included in the log file.
+	 */
 	public void registro_actividad(String ruta_archvio, String linea_entrada);
 }

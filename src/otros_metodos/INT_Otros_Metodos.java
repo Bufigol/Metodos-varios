5b62da5b8bbb70a84c60fc17845060e61a9b6a38
Felipe Andrés Vásquez Tort <felipevasquez.tort@gmail.com>
2015-11-25 11:48:47 +0100

    Actualizacion de rama.
    Ahora esta a la par que la rama master.

--- src/otros_metodos/INT_Otros_Metodos.java
+++ src/otros_metodos/INT_Otros_Metodos.java
@@ -9,7 +9,7 @@ public interface INT_Otros_Metodos {
 	/**
 	 * Metodo que a partir de una cadena de caracteres espedifica se combierte a
 	 * la fecha correspondiente
-	 * 
+	 *
 	 * @author Bufigol
 	 * @param entada
 	 *            Un string correspondiente a la fecha con el formato YYYYMMDD
@@ -17,4 +17,12 @@ public interface INT_Otros_Metodos {
 	 * @throws ParseException
 	 */
 	public Date paso_a_fecha(String entada) throws ParseException;
+
+	public void Fibonacci(int n);
+
+	public long factorial(long N);
+
+	public int[] ordenar_vector(int[] vector_entrada);
+
+	public void imprimir_vector(int[] vector);
 }
--- src/otros_metodos/Otros_Metodos.java
+++ src/otros_metodos/Otros_Metodos.java
@@ -14,8 +14,57 @@ public class Otros_Metodos implements INT_Otros_Metodos {
 		return stringFecha;
 	}
 
+	@Override
 	public Date paso_a_fecha(String entada) throws ParseException {
 		SimpleDateFormat paso_a_fecha = new SimpleDateFormat("YYYYMMDD");
 		return paso_a_fecha.parse(entada);
+	}
+
+	@Override
+	public void Fibonacci(int n) {
+		int fibo1 = 1;
+		int fibo2 = 1;
+		System.out.print(fibo1 + " ");
+		for (int i = 2; i <= n; i++) {
+			System.out.print(fibo2 + " ");
+			fibo2 = fibo1 + fibo2;
+			fibo1 = fibo2 - fibo1;
+		}
+	}
+
+	@Override
+	public long factorial(long N) {
+		if (N == 0) {
+			return 1;
+		} else {
+			return N * (factorial(N - 1));
+		}
+	}
+
+	@Override
+	public int[] ordenar_vector(int[] vector_entrada) {
+		int[] arr = vector_entrada;
+		for (int i = arr.length - 1; i > 0; i--) {
+			for (int j = 0; j < i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		return arr;
+	}
+
+	@Override
+	public void imprimir_vector(int[] vector) {
+		System.out.print("[" + vector[0] + " , ");
+		for (int i = 1; i < vector.length; i++) {
+			if (i != (vector.length - 1)) {
+				System.out.print(vector[i] + " , ");
+			} else {
+				System.out.println(vector[i] + "]");
+			}
+		}
 	}
 }
